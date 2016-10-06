6c183902a63f20841269646887020a8cd6db4dac
wangjiezhe <wangjiezhe@gmail.com>
2016-10-06 22:34:31 +0800

    Better output for `mcons`

--- tls/main.rkt
+++ tls/main.rkt
@@ -1,30 +1,15 @@
 #lang r5rs
 
-(#%provide #%module-begin
-           ;(rename racket:module-begin #%module-begin)
-           #%top-interaction
-           #%app
-           #%datum
-           define
-           lambda
-           cond
-           else
-           and
-           or
-           quote
-           car
-           cdr
-           cons
-           null?
-           atom?
-           eq?
-           add1
-           sub1
-           zero?
-           number?
+(#%provide (rename module-begin #%module-begin)
+           #%app #%datum #%top #%top-interaction  ; for program
+           define lambda cond else and or quote   ; basic keywords
+           car cdr cons null?                     ; for list
+           atom? eq?                              ; for atom
+           add1 sub1 zero? number?                ; for number
+           quasiquote unquote list                ; keywords for help
            )
 
-;(#%require (rename racket/base racket:module-begin #%module-begin))
+(#%require (only racket/base #%printing-module-begin module))
 
 (define atom?
   (lambda (x)
@@ -37,3 +22,13 @@
 (define sub1
   (lambda (n)
     (- n 1)))
+
+(define-syntax module-begin
+  (syntax-rules ()
+    ((_ . forms)
+     (#%printing-module-begin
+      (module configure-runtime '#%kernel
+        (print-as-expression #f)
+        (print-pair-curly-braces  #t)
+        (print-mpair-curly-braces #f))
+      . forms))))
